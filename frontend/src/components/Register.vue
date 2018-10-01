35318948fceb1b88c53f3807678aec996797e086
werner <wernerkotze073007@gmail.com>
2018-10-01 19:58:22 +0200

    TN-01: Changed name on Register

--- frontend/src/components/Register.vue
+++ frontend/src/components/Register.vue
@@ -13,7 +13,7 @@
            </v-list-tile-avatar>
 
            <v-list-tile-content>
-             <v-list-tile-title>John Leider</v-list-tile-title>
+             <v-list-tile-title>Werner</v-list-tile-title>
            </v-list-tile-content>
          </v-list-tile>
        </v-list>
